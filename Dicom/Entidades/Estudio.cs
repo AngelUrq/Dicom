98a2929248ca342eaa77af9d4a56538e2de6d86a
Adriana <adrianaorellana34@gmail.com>
2019-03-23 16:35:45 -0400

    Cambios en estudio

--- Dicom/Entidades/Estudio.cs
+++ Dicom/Entidades/Estudio.cs
@@ -8,10 +8,13 @@ namespace Dicom.Entidades
 {
     public class Estudio
     {
-
         public int CodigoEstudio { get; set; }
         public int CodigoPaciente { get; set; }
         public int CodigoModalidad { get; set; }
+
+        public string NumeroDeAcceso { get; set; }
+        public string MedicoDeReferencia { get; set; }
+        public string MedicoDeEjercicio { get; set; }
 
         public bool Cancelado { get; set; }
         public bool Admitido { get; set; }
@@ -19,21 +22,27 @@ namespace Dicom.Entidades
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
 
-        public Estudio(int codigoEstudio, int codigoPaciente, int codigoModalidad, bool cancelado, bool admitido, DateTime fechaInicio, DateTime fechaFin)
+        public Estudio(int codigoEstudio, int codigoPaciente, int codigoModalidad, string numeroAcceso, string medicoReferencia, string medicoEjercicio, bool cancelado, bool admitido, DateTime fechaInicio, DateTime fechaFin)
         {
             CodigoEstudio = codigoEstudio;
             CodigoPaciente = codigoPaciente;
             CodigoModalidad = codigoModalidad;
+            NumeroDeAcceso = numeroAcceso;
+            MedicoDeReferencia = medicoReferencia;
+            MedicoDeEjercicio = medicoEjercicio;
             Cancelado = cancelado;
             Admitido = admitido;
             FechaInicio = fechaInicio;
             FechaFin = fechaFin;
         }
 
-        public Estudio(int codigoPaciente, int codigoModalidad, bool cancelado, bool admitido, DateTime fechaInicio, DateTime fechaFin)
+        public Estudio(int codigoPaciente, int codigoModalidad, string numeroAcceso, string medicoReferencia, string medicoEjercicio, bool cancelado, bool admitido, DateTime fechaInicio, DateTime fechaFin)
         {
             CodigoPaciente = codigoPaciente;
             CodigoModalidad = codigoModalidad;
+            NumeroDeAcceso = numeroAcceso;
+            MedicoDeReferencia = medicoReferencia;
+            MedicoDeEjercicio = medicoEjercicio;
             Cancelado = cancelado;
             Admitido = admitido;
             FechaInicio = fechaInicio;
